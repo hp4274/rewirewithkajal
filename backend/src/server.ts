6bbe4424d199440f0c3ea2cd6b626021562fe482
Patel Harsh <harshlpatel.4274@gmail.com>
2026-03-08 10:18:47 +0530

    vercel json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -61,6 +61,10 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+if (!process.env.VERCEL) {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+export default app;
